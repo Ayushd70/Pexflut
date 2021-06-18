dcf25c6e725b27b302a5c9e91bccd3e46b69a29d
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-18 18:22:30 +0530

    lib: main: implement a theme style
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:pex_flut/router.dart';
 import 'package:flutter/material.dart';
+import 'package:pex_flut/themes.dart';
 
 void main() {
   FluroRouter.setupRouter();
@@ -11,6 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       title: 'PexFlut',
       initialRoute: 'home',
       onGenerateRoute: FluroRouter.router.generator,
